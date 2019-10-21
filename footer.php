cb94043ebb267a9b03f83a1f6ed0712a00da2d29
Dasha Fomenko <fomenko.dashka@gmail.com>
2019-10-21 17:34:16 +0300

    map, category, win blocks

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer></footer>
 
 <script src="src/js/jquery.min.js" type="text/javascript"></script>
-<script src="src/js/jquery.easytabs.min.js" type="text/javascript"></script>  
+<script src="src/js/jquery.easytabs.min.js" type="text/javascript"></script>
+<script src="src/js/swiper.min.js" type="text/javascript"></script>
 <script src="src/js/global.js" type="text/javascript"></script>
 
 </body>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 
 <div class="site-content">
-	<main id="main" role="main" tabindex="-1">
+	<main id="main" role="main" tabindex="-1" class="s-site-content__home-page">
 		<section class="o-banner o-banner--home">
 			<div class="o-banner__wrapper">
 				<div class="container">
@@ -13,10 +13,10 @@
 								<img src="img/stronger_Than_Steel_Logo.svg" alt="" class="o-banner__content-img">
 								<div class="o-banner__btn-wrap">
 									<a href="#" class="c-button c-button--primary">
-										<span class="c-button--primary__text">Watch Video</span>
+										<span class="c-button--text">Watch Video</span>
 									</a>
-									<a href="#" class="c-button c-button--primary">
-										<span class="c-button--primary__text">Increase Shop Throughput</span>
+									<a href="#" class="c-button c-button--secondary d-none d-md-flex">
+										<span class="c-button--text">Increase Shop Throughput</span>
 									</a>
 								</div>
 								<div class="o-banner__text">
@@ -59,7 +59,7 @@
 											<a href="#" class="c-aside-tab__link">General Contact Info</a>
 											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
-												<span class="c-button--primary__text">Contact Sales</span>
+												<span class="c-button--text">Contact Sales</span>
 											</a>
 										</div>
 										<div class="c-aside-tab__item">
@@ -69,7 +69,7 @@
 											<a href="#" class="c-aside-tab__link">General Contact Info</a>
 											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
-												<span class="c-button--primary__text">Contact Support</span>
+												<span class="c-button--text">Contact Support</span>
 											</a>
 										</div>
 										<div class="c-aside-tab__item">
@@ -79,7 +79,7 @@
 											<a href="#" class="c-aside-tab__link">General Contact Info</a>
 											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
-												<span class="c-button--primary__text">Contact Parts</span>
+												<span class="c-button--text">Contact Parts</span>
 											</a>
 										</div>
 									</div>
@@ -202,84 +202,309 @@
 			<a href="#" class="c-product-card">
 				<img src="img/product1.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div class="c-button c-button--primary">
+					<div class="c-button c-button--secondary">
 						<span class="c-product-card__quality">5 Machines</span>
-						<span class="c-button--primary__text">Drill Lines</span>
+						<span class="c-button--text">Drill Lines</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product2.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">3 Machines</span>
-						<span class="c-button--primary__text">Plate Processors</span>
+						<span class="c-button--text">Plate Processors</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product3.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">4 Machines</span>
-						<span class="c-button--primary__text">Angle Lines</span>
+						<span class="c-button--text">Angle Lines</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product4.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">2 Machines</span>
-						<span class="c-button--primary__text">Thermal Cutting</span>
+						<span class="c-button--text">Thermal Cutting</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product5.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">3 Machines</span>
-						<span class="c-button--primary__text">Layout Marking</span>
+						<span class="c-button--text">Layout Marking</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product6.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">4 Machines</span>
-						<span class="c-button--primary__text">Layout Marking</span>
+						<span class="c-button--text">Layout Marking</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product7.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">5 Machines</span>
-						<span class="c-button--primary__text">Shot Blasters</span>
+						<span class="c-button--text">Shot Blasters</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product8.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">4 Machines</span>
-						<span class="c-button--primary__text">Robotic Welders</span>
+						<span class="c-button--text">Robotic Welders</span>
 					</div>
 				</div>
 			</a>
 			<a href="#" class="c-product-card">
 				<img src="img/product9.png" alt="" class="c-product-card__img">
 				<div class="c-product-card__footer">
-					<div href="#" class="c-button c-button--primary">
+					<div href="#" class="c-button c-button--secondary">
 						<span class="c-product-card__quality">8 Machines</span>
-						<span class="c-button--primary__text">Material Handling</span>
+						<span class="c-button--text">Material Handling</span>
 					</div>
 				</div>
 			</a>
+		</section>
+
+		<section class="s-win">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6 col-xl-5">
+						<div class="s-win__wrapper-text">
+							<h1 class="s-win__title">Enter Today for a Chance to Win <span>FREE</span> Machinery</h1>
+							<p class="s-win__sub-title">Gift cards and other Peddinghaus merchandise included</p>
+							<div class="s-win__buttons">
+								<a href="#" class="c-button c-button--primary">
+									<span class="c-button--text">Current Giveaway</span>
+								</a>
+								<a href="#" class="c-button c-button--primary">
+									<span class="c-button--text">Other Offers</span>
+								</a>
+								<a href="#" class="c-button c-button--primary">
+									<span class="c-button--text">Past Winners</span>
+								</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-xl-7">
+						<div class="s-win__wrapper-img">
+							<div class="c-win-block">
+								<img src="img/mobile-vertical.png" alt="" class="c-win-block__img">
+								<div class="c-win-block__text">
+									<p class="c-win-block__text--sm">TEXT KEYWORD</p>
+									<div class="c-win-block__text__message">WINNER</div>
+									<p class="c-win-block__text--md">TO</p>
+									<p class="c-win-block__text--lg">69922</p>
+								</div>
+							</div>
+							<div class="s-win__footer">
+								<p class="s-win__text">Gift cards and other Peddinghaus merchandise included</p>
+								<a href="#" class="c-button c-button--primary">
+									<span class="c-button--text">Current Giveaway</span>
+								</a>
+								<p class="s-win__sub-text-desk">We want to protect your phones whenever you are working with steel.  In addition to the grand prizes, all winners will receive a free <a href="#">ZIZO</a> military grade cell phone case.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12">
+						<p class="s-win__sub-text">Normal text message rates apply. Text “STOP”to opt out. Text “HELP” for help. Up to 3 msgs / month. Terms.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="s-video-home">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-lg-8">
+						<div class="border-block">
+							<h2 class="s-video-home__title">We Are Stronger Than Steel</h2>
+						</div>
+						<p class="s-video-home__text">We do more than make machines for steel fabricators. We keep you ahead of the fabrication curve through unparalleled partnerships, constant research and development, industry-leading service, cutting-edge technology and quality components to create machines that are built to last. At Peddinghaus, our customers succeed.</p>
+					</div>
+					<div class="col-12">
+						<div class="s-video-home__video-wrapper">
+						<!-- <video src=""></video> -->
+							<img src="img/home-video.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="s-category">
+			<div class="container">
+				<div class="row justify-content-center">
+					<div class="col-12 col-xl-8">
+						<h2 class="s-category__title">Superior in Every Category Against the Competition</h2>
+						<p class="s-category__sub-text">Steel Fabricates Best on Peddinghaus Machines</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="c-category__wrap">
+							<div class="c-category__block">
+								<div class="c-category__img">
+									<img src="img/category1.png" alt="">
+								</div>
+								<div class="c-category__value">
+									<p class="c-category__value-text">
+										<span>2x</span> Strength
+									</p>
+								</div>
+							</div>
+							<div class="c-category__description">
+								<p class="c-category__description-text">
+									Built stronger to withstand the toughest material and the most taxing environments
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="c-category__wrap">
+							<div class="c-category__block">
+								<div class="c-category__img">
+									<img src="img/category2.png" alt="">
+								</div>
+								<div class="c-category__value">
+									<p class="c-category__value-text">
+										<span>1.5x</span> Speed
+									</p>
+								</div>
+							</div>
+							<div class="c-category__description">
+								<p class="c-category__description-text">
+									Faster processing power leads to increased shop throughput.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="c-category__wrap">
+							<div class="c-category__block">
+								<div class="c-category__img">
+									<img src="img/category3.png" alt="">
+								</div>
+								<div class="c-category__value">
+									<p class="c-category__value-text">
+										<span>3x</span> Accuracy
+									</p>
+								</div>
+							</div>
+							<div class="c-category__description">
+								<p class="c-category__description-text">
+									Precision enhancements eliminate erection-site rework
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-6 col-lg-3">
+						<div class="c-category__wrap">
+							<div class="c-category__block">
+								<div class="c-category__img">
+									<img src="img/category4.png" alt="">
+								</div>
+								<div class="c-category__value">
+									<p class="c-category__value-text">
+										<span>4x</span> Service
+									</p>
+								</div>
+							</div>
+							<div class="c-category__description">
+								<p class="c-category__description-text">
+									Reduced machine downtime with 24/7 support team
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="s-map">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="c-map">
+							<div class="c-map__title-block">
+								<h2 class="c-map__title">
+									Machines Worldwide
+								</h2>
+								<p class="c-map__sub-title">
+									Largest supplier of steel fabrication machinery in the world
+								</p>
+							</div>
+							<div class="c-map__img-wrap">
+								<img src="img/map-mob.png" alt="" class="c-map__img-mob">
+							</div>
+							<ul class="c-map__list">
+								<li class="c-map__item">
+									<p class="c-map__item-quality">9,000<span>+</span></p>
+									<p class="c-map__item-description">Machines</p>
+								</li>
+								<li class="c-map__item">
+									<p class="c-map__item-quality">89<span>+</span></p>
+									<p class="c-map__item-description">Countries</p>
+								</li>
+								<li class="c-map__item">
+									<p class="c-map__item-quality">7</p>
+									<p class="c-map__item-description">Branches</p>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="s-news">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="c-news-card">
+							<div class="row">
+								<div class="col-12 col-md-6 col-xl-5 order-1 order-md-2">
+									<div class="c-news-card__content">
+										<div class="c-news-card__header">
+											<p class="c-news-card__header-text">New Video Release</p>
+										</div>
+										<div class="c-news-card__body">
+											<h3 class="c-news-card__title">Clean Production with the PeddiBlast at Acier Select</h3>
+											<p class="c-news-card__text">As molor reperchil is quibus dolore il estis eni berumquas ipsunt molum inusapient la es sit. Met fugiam que.Igniet lacil iur reiciis doluptasi tendicium eum quides quanes adi moditiunt eum quat. As bus dolore il ipsunt molum inusapient la es sit, te si voluptatur ipsaudis ipsa vit.</p>
+											<a href="#" class="c-button c-button--primary">
+												<span class="c-button--text">View All Videos</span>
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-md-6 col-xl-7 order-2 order-md-1">
+									<div class="c-news-card__img">
+										<img src="img/news-video.png" alt="">
+									</div>
+									<a href="#" class="c-button c-button--primary d-md-none">
+										<span class="c-button--text">View All Videos</span>
+									</a>
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	</main>
 </div>
