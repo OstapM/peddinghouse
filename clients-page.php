bf3c5cd770c27052f105e423cb73eb4a03e1d465
catia.stroi <catia.stroi@gmail.com>
2019-10-21 17:31:43 +0300

    added section brand-cards

---
+++ clients-page.php
@@ -0,0 +1,134 @@
+<?php 
+	include ("header.php") 
+?>
+<div class="clients-content"> 
+	<main id="main" role="main" tabindex="-1">
+		<section class="s-brand-cards">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/cives.png" alt="Cives">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link">www.cives.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/lejeunesteel.png" alt="Lejeunesteel">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.lejeunesteel.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/coopersteel.png" alt="Coopersteel">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.coopersteel.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/bbironworksinc.png" alt="Bbironworksinc">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.bbironworksinc.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/acierselect.png" alt="Acierselect">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.acierselect.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/jpwcompanies.png" alt="Jpwcompanies">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.jpwcompanies.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/nafcolab.png" alt="Nafcolab">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.nafcolab.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/lemco.png" alt="Lemco">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.lemco.ca.za</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div> 
+					<div class="col-12 col-md-6 col-lg-4"> 
+						<div class="c-brand-card"> 
+							<a href="" class="c-brand-card__img">
+								<img src="img/client-brand/steelincga.png" alt="Steelincga">
+							</a> 
+							<div class="c-brand-card__footer dark-grey-bg">
+								<a href="#" class="c-link c-link__brand">www.steelincga.com</a>
+								<a class="c-button c-button--primary">
+									<span class="c-button--primary__text">Watch Video </span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> 
+	</main>
+</div> 
+
+<?php 
+	include ("footer.php") 
+?>
