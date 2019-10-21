c0cb99d44b7fb3a5e222172b9ad42a98a062e701
Dasha Fomenko <fomenko.dashka@gmail.com>
2019-10-21 11:16:00 +0300

    products section

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
 								<img src="img/stronger_Than_Steel_Logo.svg" alt="" class="o-banner__content-img">
 								<div class="o-banner__btn-wrap">
 									<a href="#" class="c-button c-button--primary">
-										<span>Watch Video</span>
+										<span class="c-button--primary__text">Watch Video</span>
 									</a>
-									<a href="#" class="c-button c-button--secondary">
-										<span>Increase Shop Throughput</span>
+									<a href="#" class="c-button c-button--primary">
+										<span class="c-button--primary__text">Increase Shop Throughput</span>
 									</a>
 								</div>
 								<div class="o-banner__text">
@@ -45,8 +45,7 @@
 							<div class="c-aside-tab__wrapper">
 								<div class="c-aside-tab__header">
 									<a href="#" class="c-aside-tab__close">
-										<span>
-											<img src="img/icons/close-button.svg" alt="">
+										<span class="c-aside__close-icon">
 										</span>
 										close
 									</a>
@@ -61,13 +60,65 @@
 											<a href="#" class="c-aside-tab__link">General Contact Info</a>
 											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
+												<span class="c-button--primary__text">Contact Sales</span>
+											</a>
+										</div>
+										<div class="c-aside-tab__item">
+											<h5>Get Support</h5>
+											<img src="img/tab.png" alt="">
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800 Press 2</p>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
+											<a href="#" class="c-button c-button--primary">
+												<span class="c-button--primary__text">Contact Support</span>
+											</a>
+										</div>
+										<div class="c-aside-tab__item">
+											<h5>Order Parts</h5>
+											<img src="img/tab.png" alt="">
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800</p>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
+											<a href="#" class="c-button c-button--primary">
+												<span class="c-button--primary__text">Contact Parts</span>
+											</a>
+										</div>
+									</div>
+								</div>
+								<div class="c-aside-tab__footer">
+									<img src="img/stronger_Than_Steel_Logo.svg" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div id="giveaway">
+						<div class="c-banner-aside__tab-content">
+							<div class="c-aside-tab__wrapper">
+								<div class="c-aside-tab__header">
+									<a href="#" class="c-aside-tab__close">
+										<span>
+											<img src="img/icons/close-button.svg" alt="">
+										</span>
+										close
+									</a>
+									<h2 class="c-aside-tab__title">Get Access to the Most Powerful Company in the Steel Industry</h2>
+								</div>
+								<div class="c-aside-tab__body">
+								<div class="c-aside-tab__list">
+										<div class="c-aside-tab__item">
+											<h5>Sales Expert</h5>
+											<img src="img/tab.png" alt="">
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800</p>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
+											<a href="#" class="c-button c-button--primary">
 												<span>Contact Sales</span>
 											</a>
 										</div>
 										<div class="c-aside-tab__item">
 											<h5>Get Support</h5>
 											<img src="img/tab.png" alt="">
-											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800</p>
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800 Press 2</p>
 											<a href="#" class="c-aside-tab__link">General Contact Info</a>
 											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
@@ -92,38 +143,6 @@
 							</div>
 						</div>
 					</div>
-					<div id="giveaway">
-						<div class="c-banner-aside__tab-content">
-							<div class="c-aside-tab__wrapper">
-								<div class="c-aside-tab__header">
-									<a href="#" class="c-aside-tab__close">
-										<span>
-											<img src="img/icons/close-button.svg" alt="">
-										</span>
-										close
-									</a>
-									<h2 class="c-aside-tab__title">Get Access to the Most Powerful Company in the Steel Industry</h2>
-								</div>
-								<div class="c-aside-tab__body">
-									<div class="c-aside-tab__list">
-										<div class="c-aside-tab__item">
-											<h5>Sales Expert</h5>
-											<img src="img/tab.png" alt="">
-											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800 Press 2</p>
-											<a href="#" class="c-aside-tab__link">Get Help From Support</a>
-											<a href="#" class="c-aside-tab__link">Global Support</a>
-											<a href="#" class="c-button c-button--primary">
-												<span>Contact Support</span>
-											</a>
-										</div>
-									</div>
-								</div>
-								<div class="c-aside-tab__footer">
-									<img src="img/stronger_Than_Steel_Logo.svg" alt="">
-								</div>
-							</div>
-						</div>
-					</div>
 					<div id="language">
 						<div class="c-banner-aside__tab-content">
 							<div class="c-aside-tab__wrapper">
@@ -137,13 +156,33 @@
 									<h2 class="c-aside-tab__title">Get Access to the Most Powerful Company in the Steel Industry</h2>
 								</div>
 								<div class="c-aside-tab__body">
-									<div class="c-aside-tab__list">
+								<div class="c-aside-tab__list">
 										<div class="c-aside-tab__item">
 											<h5>Sales Expert</h5>
 											<img src="img/tab.png" alt="">
 											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800</p>
-											<a href="#" class="c-aside-tab__link">Order Parts Online</a>
-											<a href="#" class="c-aside-tab__link">Live Chat</a>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
+											<a href="#" class="c-button c-button--primary">
+												<span>Contact Sales</span>
+											</a>
+										</div>
+										<div class="c-aside-tab__item">
+											<h5>Get Support</h5>
+											<img src="img/tab.png" alt="">
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800 Press 2</p>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
+											<a href="#" class="c-button c-button--primary">
+												<span>Contact Support</span>
+											</a>
+										</div>
+										<div class="c-aside-tab__item">
+											<h5>Order Parts</h5>
+											<img src="img/tab.png" alt="">
+											<p class="c-aside-tab__text">USA & CAN: +1 815 937 3800</p>
+											<a href="#" class="c-aside-tab__link">General Contact Info</a>
+											<a href="#" class="c-aside-tab__link">Global Sales</a>
 											<a href="#" class="c-button c-button--primary">
 												<span>Contact Parts</span>
 											</a>
@@ -158,6 +197,90 @@
 					</div>
 				</div>
 			</div>
+		</section>
+
+		<section class="s-product-cards">
+			<a href="#" class="c-product-card">
+				<img src="img/product1.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div class="c-button c-button--primary">
+						<span class="c-product-card__quality">5 Machines</span>
+						<span class="c-button--primary__text">Drill Lines</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product2.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">3 Machines</span>
+						<span class="c-button--primary__text">Plate Processors</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product3.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">4 Machines</span>
+						<span class="c-button--primary__text">Angle Lines</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product4.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">2 Machines</span>
+						<span class="c-button--primary__text">Thermal Cutting</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product5.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">3 Machines</span>
+						<span class="c-button--primary__text">Layout Marking</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product6.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">4 Machines</span>
+						<span class="c-button--primary__text">Layout Marking</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product7.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">5 Machines</span>
+						<span class="c-button--primary__text">Shot Blasters</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product8.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">4 Machines</span>
+						<span class="c-button--primary__text">Robotic Welders</span>
+					</div>
+				</div>
+			</a>
+			<a href="#" class="c-product-card">
+				<img src="img/product9.png" alt="" class="c-product-card__img">
+				<div class="c-product-card__footer">
+					<div href="#" class="c-button c-button--primary">
+						<span class="c-product-card__quality">8 Machines</span>
+						<span class="c-button--primary__text">Material Handling</span>
+					</div>
+				</div>
+			</a>
 		</section>
 	</main>
 </div>
